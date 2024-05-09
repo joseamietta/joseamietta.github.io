f8df1f8b76c688ae521ac0dc9600861bf29bcfbc
Jose Amietta <joseamietta@gmail.com>
2024-05-08 21:02:46 -0300

    deploy with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joseamietta.github.io',
   integrations: [
     tailwind({
       applyBaseStyles: true,
